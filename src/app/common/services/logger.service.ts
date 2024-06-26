64ff45babe50912f54259b0e5461107a76b5c4e2
Shane Freed <shane.freed@gmail.com>
2024-06-26 19:51:33 -0400

    added 2 services

---
+++ src/app/common/services/logger.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from "@angular/core";
+import { TracerDAO } from "../../dao/tracer.dao";
+import { LogMessage } from "../../models/util/log-message.model";
+import { Timestamp } from "@google-cloud/firestore";
+
+@Injectable({
+  providedIn: "root",
+})
+export class LoggerService {
+
+  constructor(public dao: TracerDAO<LogMessage>) {
+  }
+
+  getAll(): Promise<LogMessage[]>{    
+    return this.dao.getAll('log-messages').then(messages => {
+      messages.forEach(message => {
+        if(message.date){
+          message.date = (message.date as Timestamp).toDate();
+        }
+      })
+      return messages;
+    });
+  }
+
+  getById(id: String): Promise<LogMessage>{
+    return this.dao.getById(id, 'log-messages');
+  }
+
+  add(value: LogMessage): Promise<LogMessage>{
+    return this.dao.add(value, 'log-messages');
+  }
+
+  update(id: string, value: LogMessage): Promise<LogMessage>{
+    return this.dao.update(id, value, 'log-messages');  
+  }
+
+  delete(id: string){
+    return this.dao.delete(id, 'log-messages');
+  }
+}
