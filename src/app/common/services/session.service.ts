a53fa1b24ff8dde352aa75c1c98c5c73b53dbf8f
Shane Freed <shane.freed@gmail.com>
2024-06-26 19:55:57 -0400

    added sessionservice

---
+++ src/app/common/services/session.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { AppUser } from '../../models/admin/user.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SessionService {  
+  constructor() {}
+
+  currentUser: AppUser;
+  
+}
