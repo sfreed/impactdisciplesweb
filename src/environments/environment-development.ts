bd443d68eb49e202d932975f75636c0c9552888c
Shane Freed <shane.freed@gmail.com>
2024-08-01 15:27:59 -0400

    setup deployment code

---
+++ src/environments/environment-development.ts
@@ -0,0 +1,16 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDRfdv2XgpLQ-ll2oxpEEMyhtC75rzkP4c",
+    authDomain: "impactdisciples-a82a8.firebaseapp.com",
+    projectId: "impactdisciples-a82a8",
+    storageBucket: "impactdisciples-a82a8.appspot.com",
+    messagingSenderId: "562759240809",
+    appId: "1:562759240809:web:7d6fa117db35b887b6a6f8",
+    measurementId: "G-KJL13HB8DV"
+  },
+  domain: 'https://impactdisciples-admin.web.app/',
+  session_expires: 30
+};
+
+
