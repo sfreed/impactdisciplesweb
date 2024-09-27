051a86c8863383b52ef2537d0213d3dad135d134
Shane Freed <shane.freed@gmail.com>
2024-09-27 00:02:31 -0400

    updated config files

--- src/environments/environment-development.ts
+++ src/environments/environment-development.ts
@@ -4,7 +4,7 @@ export const environment = {
     apiKey: "AIzaSyDuxbnrsCjpHqlNauBWsaSrQHChUN-w008",
     authDomain: "impactdisciplesdev.firebaseapp.com",
     projectId: "impactdisciplesdev",
-    storageBucket: "impactdisciplesdev.appspot.com",
+    storageBucket: "impactdisciples-a82a8.appspot.com",
     messagingSenderId: "989008672868",
     appId: "1:989008672868:web:d2ee543e60c5e927260771",
     measurementId: "G-1EEHPL0SRD"
