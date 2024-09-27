38decd1a89599c0df142ee3e857630cb59e4ea24
Shane Freed <shane.freed@gmail.com>
2024-09-27 00:19:46 -0400

    added local profile

---
+++ src/environments/environment-local.ts
@@ -0,0 +1,16 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDuxbnrsCjpHqlNauBWsaSrQHChUN-w008",
+    authDomain: "impactdisciplesdev.firebaseapp.com",
+    projectId: "impactdisciplesdev",
+    storageBucket: "impactdisciples-a82a8.appspot.com",
+    messagingSenderId: "989008672868",
+    appId: "1:989008672868:web:d2ee543e60c5e927260771",
+    measurementId: "G-1EEHPL0SRD"
+  },
+  domain: 'http://localhost:4200/',
+  session_expires: 30
+};
+
+
