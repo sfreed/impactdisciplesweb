bcc3078d5108168bf7f7c5b5b0db4cda3c81254c
Shane Freed <shane.freed@gmail.com>
2024-10-05 23:00:53 -0400

    added tax rate import

--- src/app/admin-manager/admin-manager.module.ts
+++ src/app/admin-manager/admin-manager.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { LogMessagesComponent } from './log-messages/log-messages.component';
 import { UsersComponent } from './users/users.component';
 import { DxButtonModule, DxDataGridModule, DxFileUploaderModule, DxFormModule, DxHtmlEditorModule, DxListModule, DxLoadIndicatorModule,
+         DxLoadPanelModule,
          DxNumberBoxModule,
          DxPopupModule, DxSelectBoxModule, DxSwitchModule, DxTabsModule, DxTagBoxModule, DxTextBoxModule,
          DxToolbarModule,
@@ -26,6 +27,7 @@ import { EmailTemplatesComponent } from './email-templates/email-templates.compo
     DxFormModule,
     DxFileUploaderModule,
     DxHtmlEditorModule,
+    DxLoadPanelModule,
     DxListModule,
     DxLoadIndicatorModule,
     DxNumberBoxModule,
--- src/app/admin-manager/web-config/web-config.component.ts
+++ src/app/admin-manager/web-config/web-config.component.ts
@@ -2,6 +2,13 @@ import { ToastrService } from 'ngx-toastr';
 import { Component, OnInit } from '@angular/core';
 import { WebConfigModel } from 'impactdisciplescommon/src/models/utils/web-config.model';
 import { WebConfigService } from 'impactdisciplescommon/src/services/utils/web-config.service';
+import { BehaviorSubject, map, Observable } from 'rxjs';
+import { FileImportService } from 'impactdisciplescommon/src/services/utils/file-import.service';
+import { TaxRateService } from 'impactdisciplescommon/src/services/utils/tax-rate.service';
+import DataSource from 'devextreme/data/data_source';
+import CustomStore from 'devextreme/data/custom_store';
+import { Timestamp } from 'firebase/firestore';
+import { dateFromTimestamp } from 'impactdisciplescommon/src/utils/date-from-timestamp';
 
 @Component({
   selector: 'app-web-config',
@@ -9,15 +16,45 @@ import { WebConfigService } from 'impactdisciplescommon/src/services/utils/web-c
   styleUrls: ['./web-config.component.css']
 })
 export class WebConfigComponent implements OnInit{
+  datasource$: Observable<DataSource>;
+
+  public isVisible$ = new BehaviorSubject<boolean>(false);
+
   selectedItem: WebConfigModel;
 
   spinnerVisible: boolean = true;
 
   itemType = 'Web Configuration';
 
-  constructor(private service: WebConfigService, private toastr:ToastrService) {}
+  files: File[] = [];
+
+  showImportButton: boolean = false;
+
+  loadingVisible = false;
+
+  constructor(private service: WebConfigService,
+    private toastr:ToastrService,
+    private fileImportService: FileImportService,
+    private taxRateService: TaxRateService) {}
 
   async ngOnInit() {
+    this.datasource$ = this.taxRateService.streamAll().pipe(
+      map(
+        (items) =>
+          new DataSource({
+            reshapeOnPush: true,
+            pushAggregationTimeout: 100,
+            store: new CustomStore({
+              key: 'id',
+              loadMode: 'raw',
+              load: function (loadOptions: any) {
+                return items;
+              }
+            })
+          })
+      )
+    );
+
     this.selectedItem = await this.service.getAll().then(config => {
       this.spinnerVisible = false;
 
@@ -40,5 +77,39 @@ export class WebConfigComponent implements OnInit{
     this.toastr.success("Configuration Saved Successfully!");
 
     this.spinnerVisible = false;
+  }
+
+  async importTaxForm(){
+    this.loadingVisible = true;
+
+    await this.fileImportService.importRatesFiles(this.files).then(async rates => {
+      let promises = [];
+      rates.forEach(async rate => {
+        promises.push(this.taxRateService.add(rate));
+      })
+      return promises;
+    }).then(promises => {
+      Promise.all(promises).then(async () => {
+        this.selectedItem.taxImportDate = Timestamp.now();
+        this.selectedItem = await this.service.update(this.selectedItem.id, this.selectedItem);
+        this.toastr.success("Tax Rates Imported Successfully!");
+        this.loadingVisible = false;
+      })
+    })
+  }
+
+  onFileUploaded(file: File){
+    this.files.push(file);
+    this.showImportButton = true;
+  }
+
+  getLastImportedDate(){
+    if(this.selectedItem.taxImportDate){
+
+
+      return (dateFromTimestamp(this.selectedItem.taxImportDate as Timestamp) as Date).toDateString();
+    } else {
+      return '';
+    }
   }
 }
