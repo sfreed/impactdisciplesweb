7340a140e2f84137f6455b96e996ab15f95807c6
Shane Freed <shane.freed@gmail.com>
2024-10-21 14:41:30 -0400

    updates stripeKey env var name

--- src/environments/environment-production.ts
+++ src/environments/environment-production.ts
@@ -11,11 +11,14 @@ export const environment = {
   },
   domain: 'https://impactdisciples-admin.web.app',
   session_expires: 30,
-  stripeTestKey: "pk_live_51IP8IBC4Pv6WfeJrsYMjRAmzFsA0L6kjBvQx8upqGHxuo3T0WI60u1isqXBz29SyfC27v3yB34sGUYu1pl7oU1Co00LY6lIrrW",
-
+  stripeKey: "pk_live_51IP8IBC4Pv6WfeJrsYMjRAmzFsA0L6kjBvQx8upqGHxuo3T0WI60u1isqXBz29SyfC27v3yB34sGUYu1pl7oU1Co00LY6lIrrW",
+  freeEbookUrl: "https://firebasestorage.googleapis.com/v0/b/impactdisciples-a82a8.appspot.com/o/EBooks%2FM-7-Journal.pdf?alt=media&token=50e3282f-6fa1-46aa-ad3a-a486e4024af1",
   stripeCancelURL: "https://us-central1-impactdisciples-a82a8.cloudfunctions.net/cancel",
   stripeRefundURL: "https://us-central1-impactdisciples-a82a8.cloudfunctions.net/refund",
   shippingUrl: "https://us-central1-impactdisciples-a82a8.cloudfunctions.net/shipping",
+  shippingLabelUrl: "https://us-central1-impactdisciples-a82a8.cloudfunctions.net/get_shipping_label",
+  unsubscribeUrl: "https://us-central1-impactdisciples-a82a8.cloudfunctions.net/unsubscribe_from_email_list",
+  youtubeKeyUrl: "https://us-central1-impactdisciples-a82a8.cloudfunctions.net/get_youtube_keys",
   application: "admin"
 };
 
